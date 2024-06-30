a6c6ec7d1916c6aefcd3ff33198a12e8c58f1b8d
Arsallan Shahab <mdarsallan@gmail.com>
2024-06-30 10:13:33 +0530

    change to revamp

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -17,9 +17,9 @@ const Layout = (props: Props) => {
         gap="lg"
         className="hidden h-full w-full bg-black px-5 py-10 lg:flex"
       >
-        <h3 className="text-xl font-medium text-indigo-200">Kernel Krew</h3>
+        <h3 className="text-xl font-medium text-indigo-200">revamp</h3>
         <p className="text-5xl text-white">
-          Welcome to Kernel Krew,
+          Welcome to revamp,
           <br /> {pathname} to continue
         </p>
       </FlexContainer>
